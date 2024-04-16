f8586de5bedd0e3d65dc963413a8358cc26cf91c
Soliu Adeosun <sawlew@gmail.com>
2024-04-16 17:00:54 +0100

    form validation

--- src/component/Modal/Modal.jsx
+++ src/component/Modal/Modal.jsx
@@ -1,54 +1,59 @@
-import React from 'react'
-import "./modal.css"
+import React, { useState } from 'react';
+import "./modal.css";
 import { useForm, ValidationError } from '@formspree/react';
 
-const Modal = ({displayModal}) => {
-
+const Modal = ({ displayModal }) => {
+    const [submitted, setSubmitted] = useState(false);
     const [state, handleSubmit] = useForm("xdoqpvjo");
-    if (state.succeeded) {
-        return (
-            <div>
-              <p>Thanks for joining!</p>
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        await handleSubmit(event);
+        if (state.succeeded) {
+            setSubmitted(true);
+        }
+    };
+
+    return (
+        <div className='light-bg'>
+            <div data-aos="fade-right" className='main-bg'>
+                <span className="close" onClick={displayModal}>&times;</span>
+                {submitted ? (
+                    <div className='success-message'>
+                        <p>Thanks for joining!</p>
+                    </div>
+                ) : (
+                    <div>
+                        <div className='head-tag'>
+                            <h1>Coming soon!!!</h1>
+                            <p>Be the first to know when it's available</p>
+                        </div>
+                        <form onSubmit={handleFormSubmit} id='form'>
+                            <div className='form-tag'>
+                                <input type="text" placeholder='Name' name='name' id='name' required />
+                                <ValidationError
+                                    prefix="Name"
+                                    field="name"
+                                    errors={state.errors}
+                                />
+                            </div>
+                            <div className='form-tag'>
+                                <input type="email" placeholder='Email' name="email" id="email" required />
+                                <ValidationError
+                                    prefix="Email"
+                                    field="email"
+                                    errors={state.errors}
+                                />
+                            </div>
+
+                            <ValidationError className='error-message' errors={state.errors} />
+                            <input type="submit" className='submit-button' disabled={state.submitting} value="Submit" />
+                        </form>
+                    </div>
+                )}
             </div>
-          );
-    }
-
-  return (
-    <div className='light-bg'>
-        <div data-aos="fade-right" className='main-bg'>
-        <span class="close" onClick={displayModal}>&times;</span>
-            <div className='head-tag'>
-                <h1>Coming soon!!!</h1>
-                <p>Be the first to know when it's available</p>
-            </div>
-            <form method="POST" onSubmit={handleSubmit} id='form'>
-                <div className='form-tag'>
-                    {/* <label htmlFor="name">Name</label><br /> */}
-                    <input type="text" placeholder='Name' name='name' id='name' />
-                    <ValidationError 
-                        prefix="Name" 
-                        field="name"
-                        errors={state.errors}
-                    />
-                </div>
-                <div className='form-tag'>
-                    {/* <label htmlFor="email">Email</label><br /> */}
-                    <input type="email" placeholder='Email' name="email" id="email" />
-                    <ValidationError 
-                        prefix="Email" 
-                        field="email"
-                        errors={state.errors}
-                    />
-                </div>
-
-                <ValidationError errors={state.errors} />
-                <input type="submit" className='submit-button' disabled={state.submitting} value="Submit" />
-                {/* <button type="submit" ></button> */}
-
-            </form>
         </div>
-    </div>
-  )
-}
+    );
+};
 
-export default Modal
+export default Modal;
